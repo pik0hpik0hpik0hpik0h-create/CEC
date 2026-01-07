e9e4fffb33556c03b7c1e57e6b9e9fa40c97eca2
Luis David Pico Pita <pik0hpik0hpik0hpik0h@gmail.com>
2026-01-07 12:32:44 -0500

    10. COMMIT: ESTRUCTURA DE DASHBOARD COMPLETA

--- apps/portal/urls.py
+++ apps/portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.inicio, name="inicio"), #PÁGINA PRINCIPAL
     path("ingreso", views.login, name="login"), #INICIO DE SESIÓN
+    path("dashboard", views.dashboard, name="dashboard"), #DASHBOARD
 ]
--- apps/portal/views.py
+++ apps/portal/views.py
@@ -8,3 +8,7 @@ def inicio(request):
 def login(request):
     
     return render(request, "login.html")
+
+def dashboard(request):
+
+    return render(request, "dashboard.html")
