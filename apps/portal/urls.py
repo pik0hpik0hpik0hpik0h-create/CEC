0cb77bdb983509453e5480b14b8ee04f0c475244
Luis David Pico Pita <pik0hpik0hpik0hpik0h@gmail.com>
2026-01-07 20:26:55 -0500

    11. COMMIT: ASPECTO Y COMPORTAMIENTO DE BOTONES

--- apps/portal/urls.py
+++ apps/portal/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.inicio, name="inicio"), #PÁGINA PRINCIPAL
-    path("ingreso", views.login, name="login"), #INICIO DE SESIÓN
-    path("dashboard", views.dashboard, name="dashboard"), #DASHBOARD
+    path("ingreso/", views.login, name="login"), #INICIO DE SESIÓN
+    path("dashboard/", views.dashboard, name="dashboard"), #DASHBOARD
 ]
