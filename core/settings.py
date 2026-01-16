8b70748db6377fe479aa55fd231c0ad9dc2e721a
Luis David Pico Pita <pik0hpik0hpik0hpik0h@gmail.com>
2026-01-16 12:23:45 -0500

    46. COMMIT: CORECCIÓN DE CREDENCIALES A .ENV

--- core/settings.py
+++ core/settings.py
@@ -182,8 +182,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pik0hpik0hpik0hpik0h@gmail.com'
-EMAIL_HOST_PASSWORD = 'epxdqlgmsqieiakp'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 LOGGING = {
     'version': 1,
