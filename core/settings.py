ae1fc36befb5563fa65451ac3d041b0c5b514efc
Luis David Pico Pita <pik0hpik0hpik0hpik0h@gmail.com>
2026-01-16 12:37:26 -0500

    47. COMMIT: ÚLTIMA CORRECCIÓN DE EMAIL CON ERRORES

--- core/settings.py
+++ core/settings.py
@@ -182,8 +182,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGGING = {
     'version': 1,
